a4dfa63d9ba290e6e6c40885cae89da28f762efe
Anshuman Solanki <anshumansolanki@Anshumans-MacBook-Air.local>
2023-09-30 14:56:09 +0530

    chore: remove unnecessary code from client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,16 +1,13 @@
 import axios from "axios";
 import { BaseTransport, MediaTransport, MovieSearchResultTransport } from "../transports";
 
-const apikey = 'df92fdcc';
+const APIKEY = 'df92fdcc';
 
 export const getMovieDetail = (movieId: string):Promise<BaseTransport<MediaTransport>> => {
-    // const url = `http://www.omdbapi.com/?i=${movieId}&apikey=${apikey}`
-    // return axios.get(url);
-
-    return axios.get('http://www.omdbapi.com/', {
+    return axios.get('https://www.omdbapi.com/', {
         params : {
             i : movieId,
-            apikey: apikey
+            apikey: APIKEY
         }
     })
 }
@@ -23,14 +20,12 @@ export const getMoviesSearchResult = (
         searchString: string,
         pageIndex?: number
     }):Promise<BaseTransport<MovieSearchResultTransport>> => {
-    const url = `http://www.omdbapi.com/?s=${searchString}&apikey=${apikey}`
-    axios.get('http://www.omdbapi.com/')
 
     return axios.get('http://www.omdbapi.com/', {
         params : {
             s: searchString,
             ...(pageIndex ? {page: pageIndex} : {}),
-            apikey: apikey
+            apikey: APIKEY
         }
     })
 }
