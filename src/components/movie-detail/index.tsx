45a9e195f0f7dccb119a1d6ae8d48f038de0f24f
Anshuman Solanki <anshumansolanki@Anshumans-MacBook-Air.local>
2023-10-01 11:24:57 +0530

    choreL fix detail page styles

--- src/components/movie-detail/index.tsx
+++ src/components/movie-detail/index.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react"
+import React, { useContext, useEffect, useMemo, useState } from "react"
 
-import { Container, DetailRow, OuterContainer, Poster, PosterContainer, Rating, Title } from "./styles"
+import { BottomContainer, Container, DetailRow, OuterContainer, Plot, PlotTitle, Poster, PosterContainer, PosterInnerContainer, Rating, Title, TopContainer } from "./styles"
 import { useParams } from "react-router-dom"
 import { getMovieDetail } from "../../client"
 import { MediaTransport } from "../../transports"
+import { FavMoviesContext } from "../../contexts/favMoviesContext"
+import { FavButton } from "../movie-card/styles"
 
 const DEFAULT_ID = 'tt0111161'
 const sampleMovie = {
@@ -47,10 +49,32 @@ const sampleMovie = {
     "Response": "True"
 }
 
+const RatingContainer = ({movieData}:{movieData: MediaTransport}) => {
+    return (
+      <>
+        <Rating>⭐ IMDB Rating:{movieData?.imdbRating}</Rating>
+        {movieData.Ratings.map((rating, index) => {
+          return (
+            <Rating>⭐ {rating.Source}: {rating.Value}</Rating>
+          )
+        })}
+      </>
+    )
+}
+
+const getRatingContainer = (movieData: MediaTransport) => {
+  return (
+    <RatingContainer movieData={movieData}/>
+  )
+}
+
 const MovieDetailPage = () => {
   const { id } = useParams();
   const [movieId, setMovieId] = useState<string>(id ?? DEFAULT_ID)
   const [movieData, setMovideData] = useState<MediaTransport>()
+  const {favMoviesIdList, setFavMoviesIdList} = useContext(FavMoviesContext)
+  const isLiked = useMemo(() => movieData ? favMoviesIdList.includes(movieData?.imdbID): false ,[favMoviesIdList, movieData])
+  const RatingContainer = useMemo(() => movieData ? getRatingContainer(movieData) : null,[movieData])
 
   useEffect(() => {
     getMovieDetail(movieId)
@@ -59,31 +83,54 @@ const MovieDetailPage = () => {
     })
   },[movieId])
 
+  const toggleFavourite = (e:any) => {
+    e.stopPropagation();
+    if(isLiked) {
+        const newFavList:string[] = []
+        favMoviesIdList.forEach((movieId) => {
+            if(movieId !== movieData?.imdbID)
+                newFavList.push(movieId)
+        })
+        setFavMoviesIdList(newFavList)
+    } else {
+        if(movieData)
+        setFavMoviesIdList(list => [...list, movieData?.imdbID])
+    }
+}
+
   console.log(movieData)
 
   return (
     <OuterContainer>
       <Container>
-        <Title>{movieData?.Title}</Title>
-        <div>
-          <Rating>⭐ IMDB Rating:{movieData?.imdbRating}</Rating>
-          {movieData?.Ratings.map((rating, index) => {
-            return (
-              <Rating>⭐ {rating.Source}: {rating.Value}</Rating>
-            )
-          })}
-        </div>
-        <PosterContainer>
-          <Poster src={movieData?.Poster}/>
-        </PosterContainer>
-        <DetailRow>
-          <span>Language: {movieData?.Language}</span>
-          <span>Genre: {movieData?.Genre}</span>
-        </DetailRow>
-        <DetailRow>
-          <span>Runtime: {movieData?.Runtime}</span>
-          <span>Country: {movieData?.Country}</span>
-        </DetailRow>
+        <TopContainer>
+          <Title>{movieData?.Title}</Title>
+          {RatingContainer}
+          <PosterContainer>
+            <PosterInnerContainer>
+              <FavButton onClick={(e) => toggleFavourite(e)}>{isLiked ? '✓' : '+'}</FavButton>
+              <Poster src={movieData?.Poster}/>
+            </PosterInnerContainer>
+          </PosterContainer>
+        </TopContainer>
+        <BottomContainer>
+          <DetailRow>
+            <span>Language: {movieData?.Language}</span>
+          </DetailRow>
+          <DetailRow><span>Genre: {movieData?.Genre}</span></DetailRow>
+          <DetailRow>
+            <span>Runtime: {movieData?.Runtime}</span>
+            <span>Country: {movieData?.Country}</span>
+          </DetailRow>
+          <DetailRow>Director: {movieData?.Director}</DetailRow>
+          <DetailRow>Actors: {movieData?.Actors}</DetailRow>
+          <DetailRow>Writer: {movieData?.Writer}</DetailRow>
+          <PlotTitle>PLOT</PlotTitle>
+          <Plot>{movieData?.Plot}</Plot>
+          <PlotTitle>ACHEIVEMENTS</PlotTitle>
+          <DetailRow>Box Office Collection: {movieData?.BoxOffice}</DetailRow>
+          <DetailRow>Awards: {movieData?.Awards}</DetailRow>
+        </BottomContainer>
       </Container>
     </OuterContainer>
   )
--- src/components/movie-detail/styles.tsx
+++ src/components/movie-detail/styles.tsx
@@ -18,12 +18,38 @@ export const Container = styled.div(
         font-size: 14px;
         background-color: black;
         color: white
+
+        @media only screen and (min-width: 1024px) {
+            flex-direction: row !important;
+        }
+    `,
+)
+
+export const TopContainer = styled.div(
+    () => css`
+        display: flex;
+        flex-direction: column;
+        color: white;
+
+        @media only screen and (min-width: 1024px) {
+            justify-content: end;
+            padding: 1.5rem;
+        }
+    `,
+) 
+
+export const BottomContainer = styled(TopContainer)(
+    () => css`
+        @media only screen and (min-width: 1024px) {
+            justify-content: end;
+            padding: 1.5rem;
+        }
     `,
 )
 
 export const Title = styled.h1(
     () => css`
-        color: white;
+        color: #fabd07;
         font-style: lato;
     `,
 )
@@ -35,12 +61,23 @@ export const PosterContainer = styled.div(
         align-items: center;
         background-color: #1d2024;
         padding: 1rem;
+
+        @media only screen and (min-width: 768px) {
+            justify-content: start;
+        }
+    `,
+)
+
+export const PosterInnerContainer = styled.div(
+    () => css`
+        display: flex;
+        position: relative;
     `,
 )
 
 export const Poster = styled.img(
     () => css`
-        height: 400px;
+        height: 370px;
         width: 250px;
         border: 1px solid black;
     `,
@@ -60,5 +97,23 @@ export const DetailRow = styled.div(
         justify-content: space-between;
         align-items: center;
         margin: 0.25rem 0;
+    `,
+)
+
+export const PlotTitle = styled.span(
+    () => css`
+        font-size: 20px;
+        font-weight: 600; 
+        margin-top: 1.5rem;
+        color: #fabd07;
+    `,
+)
+
+export const Plot = styled.div(
+    () => css`
+        font-size: 14px;
+        line-height: 1rem;
+        font-style: italic;
+        color: #a2a3a2;
     `,
 )
